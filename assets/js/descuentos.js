b008b7d39d9d6d675f28bbdfef53c45efff09df5
graceanax <graceanax@gmail.com>
2021-08-03 23:37:38 -0600

    Funcionalidad de descuentos terminada incluida en html

--- assets/js/descuentos.js
+++ assets/js/descuentos.js
@@ -1,6 +1,25 @@
+const resultPrice = document.getElementById("ResultPrice");
 // función cálculo de precio con descuento
 function calcularPrecioConDescuento (precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
+}
+
+function obtenerPrecioConDescuento () {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    console.log(
+        {
+            priceValue,
+            discountValue,
+            precioConDescuento
+        }
+    )
+    resultPrice.innerText = precioConDescuento.toFixed(2);
 }
